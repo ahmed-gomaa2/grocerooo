fe1ef07610539fe1d71517698c998e4aff544cbf
ahmed-gomaa2 <engahmedgomaa97@gmail.com>
2020-10-19 15:42:55 +0200

    faf

--- client/src/components/storeComponents/Vegetables.js
+++ client/src/components/storeComponents/Vegetables.js
@@ -2,19 +2,6 @@ import React, {useEffect} from 'react';
 import '../css/storeComponents/Vegetables.css'
 import * as actions from '../../actions'
 import {connect} from 'react-redux'
-import tomatoes from './VegetablesImages/tomatoes.jpg'
-import potatoes from './VegetablesImages/potatoes.jpg'
-import Capsicums from './VegetablesImages/capsicums.jpg'
-import Cauliflower from './VegetablesImages/Cauliflower.jpg'
-import carrots from './VegetablesImages/carrots.jpg'
-import peppers from './VegetablesImages/peppers.jpg'
-import Cucumber from './VegetablesImages/Cucumber.jpg'
-import Gorlic from './VegetablesImages/Gorlic.jpg'
-import Onions from './VegetablesImages/Onions.jpg'
-import Okra from './VegetablesImages/Okra.jpg'
-import Peas from './VegetablesImages/Peas.jpg'
-import Corn from './VegetablesImages/Corn.jpg'
-import SpringOnions from './VegetablesImages/SpringOnions.jpg'
 import VegetableCard from './VegetableCard';
 
 const Vegetables = (props) => {
