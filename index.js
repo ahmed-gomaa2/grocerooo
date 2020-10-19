80c24b79a573270af06b32058d7365ffae5fa27a
ahmed-gomaa2 <engahmedgomaa97@gmail.com>
2020-10-19 16:33:30 +0200

    dfa

--- index.js
+++ index.js
@@ -214,15 +214,18 @@ app.get('/api/user/lists', (req, res) => {
     })
 })
 
-if(process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve(path.dirname(''))
-    app.use(express.static(__dirname + 'client/build'))
-
-    app.get('*', (req, res)=> {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
-
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const port = process.env.PORT || 4000
 
